0e294711409606786b1e814373ce650264db5147
ROCKOraboloco <rockoraboloco@gmail.com>
2023-09-02 12:29:31 -0500

    firt commit

---
+++ src/consts.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = 'Blog';
+export const SITE_TITLE_HOME = 'Inicio';
+export const SITE_DESCRIPTION = 'Una de las sensaciones que más disfruto en mi día a día es la de aprender, y este espacio servirá para almacenar lo más importante que deseo compartir.';
