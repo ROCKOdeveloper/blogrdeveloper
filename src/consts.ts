c0456a80b5af9202a87b4a24de3a03a31bee38d5
ROCKOraboloco <rockoraboloco@gmail.com>
2023-09-06 21:30:52 -0500

    first test

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 export const SITE_TITLE = 'Blog';
 export const SITE_TITLE_HOME = 'Inicio';
-export const SITE_DESCRIPTION = 'Una de las sensaciones que más disfruto en mi día a día es la de aprender, y este espacio servirá para almacenar lo más importante que deseo compartir.';
+export const SITE_DESCRIPTION = 'Una de las sensaciones más gratificantes en mi día a día es la de aprender, y este espacio está destinado a conservar y compartir lo más valioso de mis descubrimientos.';
