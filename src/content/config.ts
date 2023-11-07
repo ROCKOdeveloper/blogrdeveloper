49bdf156a45e6994ccc8f6f5cf480f4ae2953bde
ROCKOraboloco <rockoraboloco@gmail.com>
2023-11-07 15:57:47 -0500

    first checkpoint

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,20 @@
 import { defineCollection, z } from 'astro:content';
 
+// Definir una colección llamada "blog"
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
+	// Verificar el tipo del frontmatter utilizando un esquema
 	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
+		title: z.string(), // El título del artículo del blog (cadena de texto)
+		description: z.string(), // La descripción del artículo del blog (cadena de texto)
+		pubDate: z // La fecha de publicación del artículo del blog (cadena de texto o fecha)
 			.string()
 			.or(z.date())
 			.transform((val) => new Date(val)),
-		updatedDate: z
+		updatedDate: z // La fecha de actualización del artículo del blog (cadena de texto o fecha, opcional)
 			.string()
 			.optional()
 			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
+		heroImage: z.string().optional(), // La imagen destacada del artículo del blog (cadena de texto, opcional)
 	}),
 });
 
